2e6fd8a1d6556f8d416ca4c792e206657cd64fd8
cjdaly <cjdaly@locosoft.com>
2015-06-04 23:05:49 -0700

    compute neo4j home location

--- net.locosoft.fold.neo4j/src/net/locosoft/fold/neo4j/internal/Neo4jActivator.java
+++ net.locosoft.fold.neo4j/src/net/locosoft/fold/neo4j/internal/Neo4jActivator.java
@@ -11,111 +11,22 @@
 
 package net.locosoft.fold.neo4j.internal;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import net.locosoft.fold.neo4j.INeo4jService;
-
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 
 public class Neo4jActivator implements BundleActivator {
 
-	private ServiceRegistration<INeo4jService> _serviceRegistration;
-	private Neo4jMonitor _neo4jMonitor;
+	private Neo4jService _neo4jService;
 
 	public void start(BundleContext bundleContext) throws Exception {
-
-		Neo4jService neo4jService = new Neo4jService();
-		_serviceRegistration = bundleContext.registerService(
-				INeo4jService.class, neo4jService, null);
-
-		_neo4jMonitor = new Neo4jMonitor();
-		_neo4jMonitor.start();
-
 		System.out.println("starting Neo4jActivator");
+		_neo4jService = new Neo4jService(bundleContext);
+		_neo4jService.start();
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
-
-		_neo4jMonitor._stopping = true;
-		// TODO: wait for stop?
-
-		_serviceRegistration.unregister();
-
 		System.out.println("stopping Neo4jActivator");
-	}
-
-	private class Neo4jMonitor extends Thread {
-		boolean _stopping = false;
-
-		public void run() {
-			try {
-				while (!_stopping) {
-					execNeo4jCommand("/bin/ls");
-
-					Thread.sleep(10 * 1000);
-
-					execNeo4jCommand("/bin/pwd");
-
-					Thread.sleep(10 * 1000);
-				}
-			} catch (InterruptedException ex) {
-				ex.printStackTrace();
-			}
-		}
-
-		int execNeo4jCommand(String command) {
-			System.out.println("COMMAND: " + command);
-
-			int status = -1;
-			StringBuilder processOut = new StringBuilder();
-			try {
-				Process process = Runtime.getRuntime().exec(command);
-				ProcessStreamReader reader = new ProcessStreamReader(
-						process.getInputStream(), processOut);
-				reader.start();
-				status = process.waitFor();
-				reader.join();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			} catch (InterruptedException ex) {
-				ex.printStackTrace();
-			}
-
-			System.out.println("OUTPUT:");
-			System.out.println(processOut.toString());
-			return status;
-		}
-	}
-
-	private class ProcessStreamReader extends Thread {
-		private InputStream _inputStream;
-		private StringBuilder _outputBuffer;
-
-		ProcessStreamReader(InputStream inputStream, StringBuilder outputBuffer) {
-			_inputStream = inputStream;
-			_outputBuffer = outputBuffer;
-		}
-
-		public void run() {
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(_inputStream));
-
-				int readRaw = reader.read();
-				while (readRaw != -1) {
-					char c = (char) readRaw;
-					_outputBuffer.append(c);
-					readRaw = reader.read();
-				}
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
+		_neo4jService.stop();
 	}
 
 }
--- net.locosoft.fold.neo4j/src/net/locosoft/fold/neo4j/internal/Neo4jService.java
+++ net.locosoft.fold.neo4j/src/net/locosoft/fold/neo4j/internal/Neo4jService.java
@@ -11,8 +11,132 @@
 
 package net.locosoft.fold.neo4j.internal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
 import net.locosoft.fold.neo4j.INeo4jService;
 
-public class Neo4jService implements INeo4jService {
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Neo4jService implements INeo4jService, Runnable {
+
+	private BundleContext _bundleContext;
+	private String _neo4jHomeDir;
+
+	public Neo4jService(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+		_neo4jHomeDir = getNeo4jHomeDir();
+		System.out.println("neoHome: " + _neo4jHomeDir);
+	}
+
+	private String getNeo4jHomeDir() {
+		try {
+			String eclipseLocation = System
+					.getProperty("eclipse.home.location");
+			IPath eclipsePath = new Path(new URI(eclipseLocation).getPath());
+			IPath foldHomePath = eclipsePath.removeLastSegments(1)
+					.removeTrailingSeparator();
+
+			String neo4jVersion = System
+					.getProperty("net.locosoft.fold.neo4j.version");
+			IPath neo4jHomePath = foldHomePath.append("/neo4j/neo4j-community-"
+					+ neo4jVersion);
+			return neo4jHomePath.toString();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+	private ServiceRegistration<INeo4jService> _serviceRegistration;
+	private Thread _thread;
+
+	public void start() {
+		_serviceRegistration = _bundleContext.registerService(
+				INeo4jService.class, this, null);
+		_thread = new Thread(this);
+		_thread.start();
+	}
+
+	public void stop() {
+		_serviceRegistration.unregister();
+		_stopping = true;
+		// TODO: wait for _stopped?
+	}
+
+	boolean _stopping = false;
+	boolean _stopped = false;
+
+	public void run() {
+		try {
+			while (!_stopping) {
+				execNeo4jCommand("/bin/ls");
+
+				Thread.sleep(10 * 1000);
+
+				execNeo4jCommand("/bin/pwd");
+
+				Thread.sleep(10 * 1000);
+			}
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		_stopped = true;
+	}
+
+	private int execNeo4jCommand(String command) {
+		System.out.println("COMMAND: " + command);
+
+		int status = -1;
+		StringBuilder processOut = new StringBuilder();
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+			ProcessStreamReader reader = new ProcessStreamReader(
+					process.getInputStream(), processOut);
+			reader.start();
+			status = process.waitFor();
+			reader.join();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println("OUTPUT:");
+		System.out.println(processOut.toString());
+		return status;
+	}
+
+	private class ProcessStreamReader extends Thread {
+		private InputStream _inputStream;
+		private StringBuilder _outputBuffer;
+
+		ProcessStreamReader(InputStream inputStream, StringBuilder outputBuffer) {
+			_inputStream = inputStream;
+			_outputBuffer = outputBuffer;
+		}
+
+		public void run() {
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(_inputStream));
+
+				int readRaw = reader.read();
+				while (readRaw != -1) {
+					char c = (char) readRaw;
+					_outputBuffer.append(c);
+					readRaw = reader.read();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
 }
